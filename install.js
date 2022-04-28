9cc14650e20e7012a15b05a6b74173e27d4a7b3f
doremi31618 <doremi31618@gmail.com>
2022-04-28 09:55:54 +0800

    first commit

---
+++ install.js
@@ -0,0 +1,19 @@
+var Service = require('node-windows').Service;
+var fs = require('fs');
+const config = JSON.parse(fs.readFile("./config.json"));
+installService();
+
+function installService(){
+    if (config.path == "")return;
+    var new_service = new Service({
+        name: config.name,
+        description: config.description,
+        script: config.path
+    })
+    new_service.on('install', function(){
+        new_service.start();
+    })
+
+    new_service.install();
+}
+
